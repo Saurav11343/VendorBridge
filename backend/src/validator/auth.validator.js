33dba4e443e5f10787c9c7513f3a5a3805194e43
Saurav Rajput <sauravrajput11343@gmail.com>
2026-06-06 10:07:23 +0530

    auth function added (login,sinup) and validation files

---
+++ backend/src/validator/auth.validator.js
@@ -0,0 +1,39 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z
+    .string()
+    .trim()
+    .min(1, "Email is required")
+    .email("Invalid email address"),
+
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(6, "password must be at least 6 characters"),
+});
+
+export const signupSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(1, "Name is required")
+    .min(3, "Name must be at least 3 characters"),
+  email: z
+    .string()
+    .trim()
+    .min(1, "Email is required")
+    .email("Invalid email address"),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(6, "password must be at least 6 characters"),
+});
+
+export const changePasswordSchema = z.object({
+  currentPassword: z.string().min(1, "Current password is required"),
+  newPassword: z
+    .string()
+    .min(1, "Current password is required")
+    .min(6, "new Password must be at least 6 characters"),
+});
