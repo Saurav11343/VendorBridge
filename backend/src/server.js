3e28e33d8e973d7a44c0bd3217b32283016589d6
Saurav Rajput <sauravrajput11343@gmail.com>
2026-06-06 09:29:47 +0530

    mongodb connection added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { ENV } from "./utils/env.js";
 import app from "./app.js";
+import { connectDB } from "./config/db.js";
 
 const PORT = ENV.PORT;
+
+connectDB();
 
 app.listen(PORT, (req, res) => {
   console.log("Server is running on port : ", PORT);
