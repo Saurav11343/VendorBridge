82999b9fd4df52a601c4fa2cb1d815bc6cf0f94a
shaikhamaan0785 <shaikhamaan0785@gmail.com>
2026-06-06 12:16:47 +0530

    navbar and dashboard added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,29 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Signup from './components/Signup';
 import Update from './components/Updatesign';
-import Login from "../src/components/Login";
-// import Signup from './components/signup';
+import Login from "./components/Login";
+import Dashboard from './pages/Dashboard';
+import Navbar from './components/Navbar';
+import VendorDashboard from './pages/VendorDashboard';
+import ProcurementDashboard from './pages/ProcurementDashboard';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
+    <BrowserRouter>
+
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Update />} />
-        <Route path="/signup" element={<Update />}/>
-        <Route path="/login" element={<Login />}/>
-        
+        <Route path="/signup" element={<Update />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/procurement" element={<ProcurementDashboard />} />
+        <Route path="/vendor" element={<VendorDashboard />} />
       </Routes>
-    </BrowserRouter>
-    
 
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/ProcurementDashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProcurementDashboard = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Procurement Dashboard</h1>
+      <p>Manage purchase requests, vendors, and approvals.</p>
+    </div>
+  );
+};
+
+export default ProcurementDashboard;
