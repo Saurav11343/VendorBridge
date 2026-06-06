102a4d0b920e46c776b176160720fa0a2372cbbc
Saurav Rajput <sauravrajput11343@gmail.com>
2026-06-06 14:40:31 +0530

    vendor form added

--- frontend/src/pages/dashboard/VendorDashboard.jsx
+++ frontend/src/pages/dashboard/VendorDashboard.jsx
@@ -1,6 +1,27 @@
+import AddVendor from "../../components/pages/vendor/AddVendor";
+
 // pages/VendorDashboard.jsx
 function VendorDashboard() {
-  return <h1>Vendor Dashboard</h1>;
+  return (
+    <div>
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-3xl font-bold">Vendor Management</h1>
+
+        <button
+          className="btn btn-primary"
+          onClick={() =>
+            document.getElementById("add_vendor_modal").showModal()
+          }
+        >
+          + Add Vendor
+        </button>
+      </div>
+
+      <AddVendor />
+
+      {/* Vendor Table Here */}
+    </div>
+  );
 }
 
 export default VendorDashboard;
