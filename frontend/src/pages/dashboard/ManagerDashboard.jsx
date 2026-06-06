3f625164e5a12345c696690844cee38a77f0d438
Saurav Rajput <sauravrajput11343@gmail.com>
2026-06-06 14:03:40 +0530

    dashboard nug fixed

---
+++ frontend/src/pages/dashboard/ManagerDashboard.jsx
@@ -0,0 +1,6 @@
+
+function ManagerDashboard() {
+  return <div>ManagerDashboard</div>;
+}
+
+export default ManagerDashboard;
---
+++ frontend/src/pages/dashboard/ProcurementDashboard.jsx
@@ -0,0 +1,6 @@
+// pages/ProcurementDashboard.jsx
+function ProcurementDashboard() {
+  return <h1>Procurement Dashboard</h1>;
+}
+
+export default ProcurementDashboard;
