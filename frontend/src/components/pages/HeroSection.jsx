5783c3a748de23750324ac5930e511ff3bb39f8c
Sohelkhan05 <pathansohel9252@gmail.com>
2026-06-06 16:00:26 +0530

    home page updated

--- frontend/src/components/pages/HeroSection.jsx
+++ frontend/src/components/pages/HeroSection.jsx
@@ -1,16 +1,22 @@
 import { Link } from "react-router-dom";
-
+import heroImage from "../../assets/p.PNG";
 function HeroSection() {
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content text-center">
+    <div
+      className="hero min-h-screen bg-cover bg-center relative"
+      style={{
+        backgroundImage: `url(${heroImage})`,
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+
+      <div className="hero-content text-center text-white">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
+          <h1 className="text-5xl font-bold">VendorBrige System</h1>
 
           <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            A Vendor Bridge Project is a smart platform that connects companies
+            and vendors for smooth, fast, and transparent business dealings
           </p>
 
           <Link to="/login" className="btn btn-primary">
